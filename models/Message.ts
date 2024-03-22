872614764631865e248e436323562b7c6358cc21
Edwin Shi <edwinshi1021@hotmail.com>
2024-03-21 22:33:19 -0700

    feat: add Message

---
+++ models/Message.ts
@@ -0,0 +1,11 @@
+class Message {
+  message: string;
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default Message;
