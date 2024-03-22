56dc07dbe5ab5ca97fbc638b53d6c9e41f046209
Edwin Shi <edwinshi1021@hotmail.com>
2024-03-21 22:21:00 -0700

    fix: clean next auth config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,8 @@
 import NextAuth from "next-auth";
 import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
-      },
-      async authorize(credentials) {
-        return null;
-      },
-    }),
-  ],
-  callbacks: {
-    async session({ session }) {
-      return {
-        ...session,
-      };
-    },
-  },
-  pages: {
-    signIn: "/",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+  providers: [],
 };
 
 const handler = NextAuth(authOptions);
