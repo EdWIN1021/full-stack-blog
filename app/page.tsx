807f682d1db622eec3c6a1134481b5754a9c5350
Edwin Shi <edwinshi1021@hotmail.com>
2024-03-24 15:14:09 -0700

    fix: remove staff picks

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Post from "@/components/Post";
 import RecentlySaved from "@/components/RecentlySaved";
 import RecommendedTopics from "@/components/RecommendedTopics";
-import StaffPicks from "@/components/StaffPicks";
 import WhoToFollow from "@/components/WhoToFollow";
 import { Container, Stack } from "@mui/material";
 import Grid from "@mui/material/Grid";
@@ -24,7 +23,6 @@ export default function Home() {
 
         <Grid item xs={4} className="p-10">
           <Stack spacing={6}>
-            <StaffPicks />
             <RecommendedTopics />
             <WhoToFollow />
             <RecentlySaved />
